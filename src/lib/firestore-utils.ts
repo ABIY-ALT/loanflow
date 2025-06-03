9f8577905c6a2bbdb46c118a6de2c933d32aff8f
Biruk Zegeju <biruk.zegeju@gmail.com>
2025-06-03 10:30:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Internal Server Error

--- src/lib/firestore-utils.ts
+++ src/lib/firestore-utils.ts
@@ -49,8 +49,7 @@ export function convertTimestampsToISO(data: any, depth = 0, maxDepth = 15, seen
   // 7. Recursive processing based on object type for objects that made it past earlier checks.
   if (Array.isArray(data)) {
     res = data.map(item => convertTimestampsToISO(item, depth + 1, maxDepth, currentSeenSet));
-  } else if (data.constructor === Object || Object.getPrototypeOf(data) === null || (typeof Object.getPrototypeOf(data) === 'object' && Object.getPrototypeOf(Object.getPrototypeOf(data)) === null) ) {
-    // Plain JavaScript object
+  } else if (data.constructor === Object) { // Simplified plain object check
     res = {};
     for (const key in data) {
       if (Object.prototype.hasOwnProperty.call(data, key)) {
@@ -62,11 +61,7 @@ export function convertTimestampsToISO(data: any, depth = 0, maxDepth = 15, seen
         }
       }
     }
-  }
-  // NOTE: The general `else if (typeof data.toDate === 'function')` block has been removed.
-  // Objects that would have fallen here and are not plain objects or arrays
-  // will now be caught by the final 'else' case.
-  else {
+  } else {
     // Unhandled complex object type (not Array, not plain Object, not Timestamp/Date, not SDK object).
     // Return as is. Cycle/depth checks on `data` itself should prevent issues.
     res = data;
@@ -76,4 +71,3 @@ export function convertTimestampsToISO(data: any, depth = 0, maxDepth = 15, seen
   currentSeenSet.delete(data);
   return res;
 }
-
